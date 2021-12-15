2947a1cfe43cfcdcac480ef3c2c25b6d3e5ff9e8
James Chang <jameschang0809@gmail.com>
2021-12-15 14:13:44 +0800

    added dependency spring-boot-configuration-processor

--- config/src/main/java/tw/papajames/spring/config/ImmutableConfigProperties.java
+++ config/src/main/java/tw/papajames/spring/config/ImmutableConfigProperties.java
@@ -2,7 +2,6 @@ package tw.papajames.spring.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "mail2")
 @ConstructorBinding
